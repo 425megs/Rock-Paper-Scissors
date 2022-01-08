990c2f70e2b3cbedc4427ae95de4365f7d67ec3e
Megan McLean <425megs@gmail.com>
2022-01-08 13:32:29 -0800

    added live site link

---
+++ script.js
@@ -0,0 +1,38 @@
+// collect user choice
+//     ask user: R, P or S
+//     store that choice somewhere
+//     edge case: user enters something other than R,P,S
+// collect computer choice
+//     Randomly select R,P,S
+//     store in variable
+// compare choices 
+//     win conditions: User R Comp S, User P Comp R, User S Comp P
+//     lost condition: User R Comp P, User P Comp S, User S Comp R 
+//     tie condition: anything else
+//     update win, loss or tie variable with result
+// Display winner
+// show statistics
+//     tracking variables for W, L, T
+//     Show to user, use multiline alert
+// option to play again
+//     after stats shown, ask user if they want to play again
+//     if yes, start the loop over
+
+
+// Need a pop up where user can enter R, P or S
+if (window.confirm("Do you want to play a game? 💀")) {
+    window.open();
+  } else {
+    (window.open("exit.html", "Thanks for Playing!");
+}
+// window.alert("Do you want to play a game? 💀");
+//     alert("Do you want to play a game? 💀");
+
+// If user selects R 
+//     else if P 
+//     else if S
+//     else to cover people entering anything else
+
+// Randomly select R, P, S
+
+// Asking them if they want to play again
